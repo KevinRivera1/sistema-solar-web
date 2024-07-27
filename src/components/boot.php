56f4e497dd7d71b57c1c7b48e400b9ba6267e5d0
Kevin Rivera <shimpo11.kr@gmail.com>
2024-07-27 16:56:40 -0500

    feat: Agregar componente de boot para mostrar un botón flotante con imagen

---
+++ src/components/boot.php
@@ -0,0 +1,9 @@
+<?php
+$image_url = isset($image_url) ? $image_url : './assets/img/bot.png';
+?>
+
+<section class="boot-float">
+    <a href="#planetas" class="boot-float__link">
+        <img src="<?php echo $image_url; ?>" alt="Subir" class="boot-float__icon">
+    </a>
+</section>
