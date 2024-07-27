0138e062cd1246642af71afccd4121afcdecbddb
Kevin Rivera <shimpo11.kr@gmail.com>
2024-07-27 16:57:07 -0500

    feat: Agregar componente de footer con información del autor

---
+++ src/components/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="footer__container">
+        <p class="footer__text">Autor: <span class="footer__name">Alisson Cachaguay</span> | © <span id="current-year"></span></p>
+    </div>
+</footer>
