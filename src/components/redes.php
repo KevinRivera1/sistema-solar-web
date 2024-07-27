a2c5f889de4f6c69857c2f40a4149a27047e1d50
Kevin Rivera <shimpo11.kr@gmail.com>
2024-07-27 16:57:21 -0500

    feat: Agregar componente de redes sociales en la página principal

---
+++ src/components/redes.php
@@ -0,0 +1,22 @@
+<section class="banner__redes">
+    <div class="banner__redes-container">
+        <h2 class="banner__redes-title">Sigue nuestras redes sociales sobre el Sistema Solar</h2>
+        <ul class="banner__redes-list">
+            <li class="banner__redes-item">
+                <a href="https://facebook.com" class="banner__redes-link banner__redes-link--facebook" aria-label="Facebook">
+                    <img src="./assets/icons/facebook.svg" alt="Icon facebook" class="banner__redes-icon">
+                </a>
+            </li>
+            <li class="banner__redes-item">
+                <a href="https://twitter.com" class="banner__redes-link banner__redes-link--twitter" aria-label="Twitter">
+                    <img src="./assets/icons/twitter.svg" alt="Icon twitter" class="banner__redes-icon">
+                </a>
+            </li>
+            <li class="banner__redes-item">
+                <a href="https://instagram.com" class="banner__redes-link banner__redes-link--instagram" aria-label="Instagram">
+                    <img src="./assets/icons/instagram.svg" alt="Icon instagram" class="banner__redes-icon">
+                </a>
+            </li>
+        </ul>
+    </div>
+</section>
