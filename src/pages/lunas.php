3e138a0445ffe560e7d909e43e51680bee9f2438
Kevin Rivera <shimpo11.kr@gmail.com>
2024-07-27 16:59:25 -0500

    feat: Agregar seccion de lunas

---
+++ src/pages/lunas.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>🌑 Lunas del sistema solar 🌜</title>
+    <link rel="stylesheet" href="../../styles/reset.css">
+    <link rel="stylesheet" href="../../styles/header.css">
+    <link rel="stylesheet" href="../../styles/lunas.css">
+    <link rel="stylesheet" href="../../styles/boot.css">
+    <link rel="stylesheet" href="../../styles/redes.css">
+    <link rel="stylesheet" href="../../styles/footer.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+        <?php require '../components/header.php'; ?>
+        <section class="moons">
+            <h2 class="moons__title">¡Descubre las Lunas del Sistema Solar!</h2>
+            <div class="moons__list">
+                <article class="moons__item" data-aos="flip-up">
+                    <img src="../../assets/lunas/luna.png" alt="luna de la tierra">
+                    <div class="moons__content">
+                        <h3 class="moons__name">Luna</h3>
+                        <p class="moons__description">La Luna es el satélite natural de la Tierra. ¡Es nuestro compañero brillante en el cielo nocturno!</p>
+                        <ul class="moons__facts">
+                            <li class="moons__fact">¿Sabías que la Luna influye en las mareas del océano?</li>
+                            <li class="moons__fact">Tiene montañas, valles y hasta cráteres.</li>
+                            <li class="moons__fact">¡Es el lugar donde los humanos han caminado fuera de nuestro planeta!</li>
+                        </ul>
+                    </div>
+                </article>
+
+                <article class="moons__item" data-aos="fade-down">
+                    <img src="../../assets/lunas/europa.png" alt="luna de jupiter">
+                    <div class="moons__content">
+                        <h3 class="moons__name">Europa</h3>
+                        <p class="moons__description">Europa es una luna de Júpiter. Se cree que tiene un océano bajo su superficie. ¡Podría haber vida allí!</p>
+                        <ul class="moons__facts">
+                            <li class="moons__fact">Es una de las lunas más brillantes del sistema solar.</li>
+                            <li class="moons__fact">Su superficie está cubierta de hielo, ¡podría haber agua líquida debajo!</li>
+                            <li class="moons__fact">Los científicos están muy emocionados por explorar Europa.</li>
+                        </ul>
+                    </div>
+                </article>
+
+                <article class="moons__item" data-aos="fade-up">
+                    <img src="../../assets/lunas/titan.png" alt="luna de saturno">
+                    <div class="moons__content">
+                        <h3 class="moons__name">Titán</h3>
+                        <p class="moons__description">Titán es la luna más grande de Saturno. Tiene una atmósfera densa y lagos de metano.</p>
+                        <ul class="moons__facts">
+                            <li class="moons__fact">Titán es más grande que el planeta Mercurio.</li>
+                            <li class="moons__fact">¡Es el único lugar en el sistema solar conocido por tener líquidos en su superficie!</li>
+                            <li class="moons__fact">Podría tener las condiciones adecuadas para la vida.</li>
+                        </ul>
+                    </div>
+                </article>
+
+                <article class="moons__item" data-aos="fade-down">
+                    <img src="../../assets/lunas/fobos.png" alt="luna de marte">
+                    <div class="moons__content">
+                        <h3 class="moons__name">Fobos y Deimos</h3>
+                        <p class="moons__description">Fobos y Deimos son las lunas de Marte. Son pequeñas y tienen forma irregular.</p>
+                        <ul class="moons__facts">
+                            <li class="moons__fact">Fobos se acerca a Marte más que cualquier luna a su planeta.</li>
+                            <li class="moons__fact">Se cree que son asteroides atrapados por la gravedad de Marte.</li>
+                            <li class="moons__fact">¡Tienen cráteres extraños que los hacen lucir como rocas!</li>
+                        </ul>
+                    </div>
+                </article>
+
+                <article class="moons__item" data-aos="fade-up">
+                    <img src="../../assets/lunas/titania.png" alt="luna de urano">
+                    <div class="moons__content">
+                        <h3 class="moons__name">Titania</h3>
+                        <p class="moons__description">Titania es una de las lunas de Urano. Es la luna más grande y brillante del planeta.</p>
+                        <ul class="moons__facts">
+                            <li class="moons__fact">Titania es la octava luna más grande del sistema solar.</li>
+                            <li class="moons__fact">¡Tiene un cráter gigante que cubre casi un tercio de su superficie!</li>
+                            <li class="moons__fact">¡Es una de las lunas más antiguas del sistema solar!</li>
+                        </ul>
+                    </div>
+                </article>
+
+                <article class="moons__item" data-aos="fade-down">
+                    <img src="../../assets/lunas/triton.png" alt="luna de neptuno">
+                    <div class="moons__content">
+                        <h3 class="moons__name">Tritón</h3>
+                        <p class="moons__description">Tritón es la luna más grande de Neptuno. ¡Es una luna muy fría y activa!</p>
+                        <ul class="moons__facts">
+                            <li class="moons__fact">Tritón es la única luna grande en el sistema solar que orbita en dirección opuesta a la rotación de su planeta.</li>
+                            <li class="moons__fact">¡Tiene géiseres que lanzan nitrógeno líquido a su superficie!</li>
+                            <li class="moons__fact">¡Tritón es tan frío que tiene hielo de agua y hielo de nitrógeno en su superficie!</li>
+                        </ul>
+                    </div>
+                </article>
+
+                <article class="moons__item" data-aos="fade-up">
+                    <img src="../../assets/lunas/caronte.png" alt="luna de pluton">
+                    <div class="moons__content">
+                        <h3 class="moons__name">Caronte</h3>
+                        <p class="moons__description">Caronte es la luna más grande de Plutón. ¡Es tan grande que Plutón y Caronte orbitan alrededor de un punto en el espacio!</p>
+                        <ul class="moons__facts">
+                            <li class="moons__fact">Caronte es casi la mitad del tamaño de Plutón.</li>
+                            <li class="moons__fact">¡Tiene un cañón gigante que es tan largo como el Gran Cañón de la Tierra!</li>
+                            <li class="moons__fact">¡Caronte y Plutón están tan cerca que se ven gigantes en el cielo uno del otro!</li>
+                        </ul>
+                    </div>
+                </article>
+            </div>
+        </section>
+
+        <?php $image_url = '../../assets/img/bot.png';
+        require '../components/boot.php'; ?>
+        <?php require '../components/footer.php'; ?>
+    </main>
+</body>
+
+<script src="../../js/index.js"></script>
+<script src="../../js/footer.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script>
+    AOS.init();
+</script>
+
+</html>
