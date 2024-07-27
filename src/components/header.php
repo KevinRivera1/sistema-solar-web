6b479b26b159e79c10ba44b243e6f2f69a45fa48
Kevin Rivera <shimpo11.kr@gmail.com>
2024-07-27 16:56:53 -0500

    feat: Agregar componente de encabezado con logo y menú de navegación

---
+++ src/components/header.php
@@ -0,0 +1,24 @@
+<header class="header">
+    <div class="header__container">
+        <div class="header__logo">
+            <a href="#" class="header__logo-link">
+                <img src="../../assets/img/logo.png" alt="Logo" class="header__logo-image">
+            </a>
+        </div>
+        <div class="header__toggle">
+            <button class="header__menu-btn" id="menu-btn">
+                &#9776;
+            </button>
+        </div>
+        <nav class="header__nav" id="nav-menu">
+            <ul class="header__nav-list" id="nav-menu">
+                <li class="header__nav-item"><a href="../../index.php" class="header__nav-link">Inicio</a></li>
+                <li class="header__nav-item"><a href="../../index.php#planetas" class="header__nav-link">Planetas</a></li>
+                <li class="header__nav-item"><a href="../pages/lunas.php" class="header__nav-link">Lunas</a></li>
+                <li class="header__nav-item"><a href="../pages/exploracion.php" class="header__nav-link">Exploración</a></li>
+                <li class="header__nav-item"><a href="../pages/contacto.php" class="header__nav-link">Contacto</a></li>
+                <li class="header__nav-item"><a href="./src/pages/contacto.php" class="header__nav-link">Juegos</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
